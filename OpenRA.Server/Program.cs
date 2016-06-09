b597dbf679f1745aa66b676ae0d5c699d6d01337
Sander Keemink <swkeemink@scimail.eu>
2016-06-09 22:47:56 +0100

    BROKEN: trying to get server to fill bot slots from start, and start empty game
    Without running graphics

--- OpenRA.Server/Program.cs
+++ OpenRA.Server/Program.cs
@@ -14,10 +14,23 @@ using System.Net;
 using System.Threading;
 using OpenRA.Support;
 
+
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using OpenRA.Graphics;
+using OpenRA.Network;
+using OpenRA.Server;
+using OpenRA.Traits;
+using S = OpenRA.Server.Server;
+
 namespace OpenRA.Server
 {
 	class Program
 	{
+
+
+
 		static void Main(string[] args)
 		{
 			Log.AddChannel("debug", "dedicated-debug.log");
@@ -39,8 +52,51 @@ namespace OpenRA.Server
 			Console.WriteLine("[{0}] Starting dedicated server for mod: {1}", DateTime.Now.ToString(settings.TimestampFormat), mod);
 			while (true)
 			{
-				var server = new Server(new IPEndPoint(IPAddress.Any, settings.ListenPort), settings, modData, true);
+				var server = new Server(new IPEndPoint(IPAddress.Any, settings.ListenPort), settings, modData, false);
+				// add bots, as in orderManager.IssueOrder(Order.Command("slot_bot {0} {1} {2}".F(slot.Key, botController.Index, bot)));
 
+//				string slot_name = "Multi1";
+//				var slot = server.LobbyInfo.Slots[slot_name];
+//				var bot = server.LobbyInfo.ClientInSlot(slot_name);
+//				int controllerClientIndex=0;
+//
+//				var botType = "Rush AI";
+//
+//				slot.Closed = false;
+//				if (bot == null)
+//				{
+//					// Create a new bot
+//					bot = new Session.Client()
+//					{
+//						Index = server.ChooseFreePlayerIndex(),
+//						Name = botType,
+//						Bot = botType,
+//						Slot = slot_name,
+//						Faction = "Random",
+//						SpawnPoint = 0,
+//						Team = 0,
+//						State = Session.ClientState.NotReady,
+//						BotControllerClientIndex = controllerClientIndex
+//					};
+//
+//					// Pick a random color for the bot
+//					//var validator = server.ModData.Manifest.Get<ColorValidator>();
+////					var tileset = server.Map.Rules.TileSet;
+////					var terrainColors = tileset.TerrainInfo.Where(ti => ti.RestrictPlayerColor).Select(ti => ti.Color);
+////					var playerColors = server.LobbyInfo.Clients.Select(c => c.Color.RGB)
+////						.Concat(server.Map.Players.Players.Values.Select(p => p.Color.RGB));
+//					bot.Color = bot.PreferredColor;// = validator.RandomValidColor(server.Random, terrainColors, playerColors);
+//					//bot.Color = "215,113,106";
+//					server.LobbyInfo.Clients.Add(bot);
+//				}
+//
+
+
+
+
+
+				// start game
+				server.StartGame ();
 				while (true)
 				{
 					Thread.Sleep(1000);
